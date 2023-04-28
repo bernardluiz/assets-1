c05c3cf74e953b3023e6c9d505a7eb1579e8857d
BernzLuiz <131265365+BernzLuiz@users.noreply.github.com>
2023-04-28 19:16:37 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,1 +1,1 @@
-<div style="display:none"><a href="https://sidoli.batik.go.id/sg/">slot gacor</a><a href="https://jdih-setwan.tanahbumbukab.go.id/sd/">slot dana</a><a href="https://spbe.naganrayakab.go.id/sdp/">slot pulsa</a></div>
+<div style="display:none"><a href="https://sidoli.batik.go.id/sg/">slot gacor</a><a href="https://jdih-setwan.tanahbumbukab.go.id/sd/">slot dana</a><a href="https://spbe.naganrayakab.go.id/sdp/">slot pulsa</a><a href="https://ofes.itda.ac.id/sd/">https://ofes.itda.ac.id/sd/</a></div>
