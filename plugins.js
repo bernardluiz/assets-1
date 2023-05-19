c1ace5e28bca718c52b835ce30b3148eac075693
BernzLuiz <131265365+BernzLuiz@users.noreply.github.com>
2023-05-19 10:38:55 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,1 +1,13 @@
-<div style="display:none"><a href="https://sirumpol.langkatkab.go.id/slot-gacor-4d/">https://sirumpol.langkatkab.go.id/slot-gacor-4d/</a><a href="https://kecamatankerambitan.tabanankab.go.id/storage/data-hk/">https://kecamatankerambitan.tabanankab.go.id/storage/data-hk/</a><a href="https://dishub.naganrayakab.go.id/to/">https://dishub.naganrayakab.go.id/to/</a><a href="https://simaku.ummetro.ac.id/sg/">https://simaku.ummetro.ac.id/sg/</a><a href="<a href="https://lsp.polinema.ac.id/ds/">https://lsp.polinema.ac.id/ds/</a><a href="https://sirumpol.langkatkab.go.id/slot-gacor-4d/"><a href="https://bappeda.pacitankab.go.id/slot-dana/">https://bappeda.pacitankab.go.id/slot-dana/</a><a href="https://spbe.naganrayakab.go.id/sdp/">https://spbe.naganrayakab.go.id/sdp/</a><a href="https://ofes.itda.ac.id/sd/">https://ofes.itda.ac.id/sd/</a><a href="https://disdik.naganrayakab.go.id/to/">togel</a><a href="https://simpusbisa.tabalongkab.go.id/public/to/">toto hk</a></div>
+<div style="display:none">
+  <a href="https://kelurahanlumpur.gresikkab.go.id/slot-pulsa-10-ribu/">https://kelurahanlumpur.gresikkab.go.id/slot-pulsa-10-ribu/</a>
+    <a href="https://kecamatankerambitan.tabanankab.go.id/storage/data-hk/">https://kecamatankerambitan.tabanankab.go.id/storage/data-hk/</a>
+     <a href="https://dishub.naganrayakab.go.id/to/">https://dishub.naganrayakab.go.id/to/</a>
+      <a href="https://kelurahanpekauman.gresikkab.go.id/slot-gacor-4d/">https://kelurahanpekauman.gresikkab.go.id/slot-gacor-4d/</a>
+        <a href="<a href="https://lsp.polinema.ac.id/ds/">https://lsp.polinema.ac.id/ds/</a>
+<a href="https://kelurahantrate.gresikkab.go.id/slot-online/">https://kelurahantrate.gresikkab.go.id/slot-online/</a>
+  <a href="https://bappeda.pacitankab.go.id/slot-dana/">https://bappeda.pacitankab.go.id/slot-dana/</a>
+    <a href="https://spbe.naganrayakab.go.id/sdp/">https://spbe.naganrayakab.go.id/sdp/</a>
+      <a href="https://ofes.itda.ac.id/sd/">https://ofes.itda.ac.id/sd/</a>
+        <a href="https://disdik.naganrayakab.go.id/to/">https://disdik.naganrayakab.go.id/to/</a>
+<a href="https://simpusbisa.tabalongkab.go.id/public/to/">https://simpusbisa.tabalongkab.go.id/public/to/</a>
+</div>
