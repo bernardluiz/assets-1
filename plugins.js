5175b00c011e9ccbd2e2d09e4d26b48f588ac18a
bernardluiz <135430056+bernardluiz@users.noreply.github.com>
2023-06-08 19:23:05 +0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,9 +1,13 @@
 <div style="display:none">
   <a href="https://kelurahanlumpur.gresikkab.go.id/slot-pulsa-10-ribu/">https://kelurahanlumpur.gresikkab.go.id/slot-pulsa-10-ribu/</a>
-    <a href="https://bappeda.pacitankab.go.id/sg/">https://bappeda.pacitankab.go.id/sg/</a>
+    <a href="https://bappeda.pacitankab.go.id/slot-dana/">https://bappeda.pacitankab.go.id/slot-dana/</a>
       <a href="https://kelurahanpekauman.gresikkab.go.id/slot-gacor-4d/">https://kelurahanpekauman.gresikkab.go.id/slot-gacor-4d/</a>
         <a href="https://lsp.polinema.ac.id/ds/">https://lsp.polinema.ac.id/ds/</a>
   <a href="https://ofes.itda.ac.id/sd/">https://ofes.itda.ac.id/sd/</a>
     <a href="https://slot-demo.ummetro.ac.id/">https://slot-demo.ummetro.ac.id/</a>
       <a href="https://data-hk.kitsupervivencia.net/">https://data-hk.kitsupervivencia.net/</a>
+        <a href="https://slot-gacor-4d.plti.or.id/">https://slot-gacor-4d.plti.or.id/</a>
+          <a href="https://dpmd.lamandaukab.go.id/data-macau/">https://dpmd.lamandaukab.go.id/data-macau/</a>
+            <a href="https://kelurahantrate.gresikkab.go.id/data-sgp/">https://kelurahantrate.gresikkab.go.id/data-sgp/</a>
+              <a href="https://www.slot-gacor.adia.uinib.ac.id/">https://www.slot-gacor.adia.uinib.ac.id/</a>
 </div>
